03580cd36f77f8218cda48c62bcb7f175b2e3629
mamta925 <mamtarajput925@gmail.com>
2022-09-25 11:03:15 +0530

    java source file structure

---
+++ DeclarationAndAccessModifier/SourceFileSTructure/B.java
@@ -0,0 +1,9 @@
+class A {
+
+}
+public class C {  // class C is public, should be declared in a file named C.java
+    
+}
+public class B {
+    
+}
---
+++ DeclarationAndAccessModifier/SourceFileSTructure/Test.java
@@ -0,0 +1,9 @@
+ import static java.lang.Integer.MAX_VALUE;
+public class Test {
+
+ public static void main(String[] args) {
+     final int MAX_VALUE = 10;
+        System.out.println(MAX_VALUE);
+    }
+    
+}

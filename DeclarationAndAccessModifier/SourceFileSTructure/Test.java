0fb4aed38d9f4777a5e4e0ac8e4dfd892a8c2db5
mamta925 <mamtarajput925@gmail.com>
2022-09-27 21:51:17 +0530

    NodeJS Coding Problem

--- DeclarationAndAccessModifier/SourceFileSTructure/Test.java
+++ DeclarationAndAccessModifier/SourceFileSTructure/Test.java
@@ -1,9 +1,14 @@
- import static java.lang.Integer.MAX_VALUE;
-public class Test {
-
- public static void main(String[] args) {
-     final int MAX_VALUE = 10;
-        System.out.println(MAX_VALUE);
+class Test{
+    public static void main(String[] args) {
+        switch (1) {
+            case 1:
+                {
+                    break;
+                }
+               
+         
+            default:
+                break;
+         }
     }
-    
 }
